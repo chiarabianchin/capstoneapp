ae0a3e3d1227900001e4d57781ceeed25ea6d008
Chiara Bianchin <chiara.bianchin@b12-consulting.com>
2018-03-16 10:22:46 +0100

    Working on upload page, updated requirements

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 
 #flask
 from flask import Blueprint
@@ -10,3 +11,4 @@ myforms = Blueprint("forms", __name__)
 class PhotoForm(FlaskForm):
     photo = FileField("Upload Image ", validators=[FileRequired(),
                                   FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField('Upload')
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 #general
 import os
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 
 #forms
 from .forms import PhotoForm
@@ -13,8 +13,8 @@ myapp = Blueprint("app", __name__)
 
 @myapp.route("/")
 def index():
-
-    return render_template("index.html")
+    form = PhotoForm()
+    return render_template("index.html", form=form)
 
 
 @myapp.route("/info")
@@ -22,19 +22,21 @@ def info():
     return render_template("info.html")
 
 
-@myapp.route('/upload', methods=['GET', 'POST'])
+@myapp.route('/upload', methods=['POST'])
 def upload():
-    form = PhotoForm()
-    if form.validate_on_submit():
-        f = form.photo.data
-        print(f)
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(
-            myapp.instance_path, 'photos', filename
-        ))
-        return redirect(url_for('error'))
+    flash(request.form)
+    #if form.validate_on_submit():
+        #f = form.photo.data
+        #print(f)
+        #if f:
+            #flash("something loaded")
+        #if form.submit:
+            #flash("submitted")
+            #return render_template("upload.html", form=form)
+        #else:
+            #return redirect(url_for('error'))
 
-    return render_template('upload.html', form=form)
+    return render_template('upload.html', form=request.form)
 
 
 @myapp.route("/run")
@@ -45,4 +47,12 @@ def run():
 
 @myapp.route("/error")
 def error():
-    return "Error"
+    return render_template("error.html")
+
+
+
+
+# 1) come stampare l'output
+# 2) ottenere dalla richiesta il form
+# 3) ottenere il dato dell'immagine "form.photo"-like
+# 4) copiare l'immagine sul server, come?
